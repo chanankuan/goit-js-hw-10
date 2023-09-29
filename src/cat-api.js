cd4259e0e48a7c99d1af13d710547bca27eb90dd
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-09-29 22:14:46 +0300

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,25 +1,14 @@
 import axios from 'axios';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { API_KEY, BASE_URL } from './refs';
 
 axios.defaults.headers.common['x-api-key'] = API_KEY;
 
 export async function getBreeds() {
-  try {
-    const response = await axios.get('https://api.thecatapi.com/v1/breeds');
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    Notify.failure('Oops! Something went wrong! Try reloading the page!');
-  }
+  const response = await axios.get('https://api.thecatapi.com/v1/breeds');
+  return response.data;
 }
 
 export async function getCatByBreed(breedId) {
-  try {
-    const response = await axios.get(`${BASE_URL}?breed_id=${breedId}`);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    Notify.failure('Oops! Something went wrong! Try reloading the page!');
-  }
+  const response = await axios.get(`${BASE_URL}?bsreed_id=${breedId}`);
+  return response.data;
 }
